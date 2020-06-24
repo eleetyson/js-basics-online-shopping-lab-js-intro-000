d92101f1486dbbe3360296d8f64536f60bc50318
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-06-24 18:23:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -79,9 +79,7 @@ function removeFromCart(item) {
     }
   }
   // if the cart doesn't contain a matching item, return this instead
-  else {
     return "That item is not in your cart";
-  }
 }
 
 function placeOrder(cardNumber) {
